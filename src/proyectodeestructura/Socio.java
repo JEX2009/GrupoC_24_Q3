b35f52de016b6fc3a139f97da33b51cc2f31b4b5
JEX2009 <orozcojason816@gmail.com>
2024-10-25 13:19:48 -0600

    Se agrega la clase Socio el cual es un objeto simulando un comprador

---
+++ src/proyectodeestructura/Socio.java
@@ -0,0 +1,47 @@
+package Java.GrupoC_24_Q3.src.proyectodeestructura;
+
+public class Socio {
+    private String Nombre;
+    private String Dirección;
+    private int Numero;
+    private String NumeroSocio; // Se puede hacer una combinacion de letras y numeros
+    private Integer Edad;
+
+    public Socio(String nombre, String dirección, int numero, String numeroSocio, Integer edad) {
+        Nombre = nombre;
+        Dirección = dirección;
+        Numero = numero;
+        NumeroSocio = numeroSocio;
+        Edad = edad;
+    }
+    public String getNombre() {
+        return Nombre;
+    }
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    public String getDirección() {
+        return Dirección;
+    }
+    public void setDirección(String dirección) {
+        Dirección = dirección;
+    }
+    public int getNumero() {
+        return Numero;
+    }
+    public void setNumero(int numero) {
+        Numero = numero;
+    }
+    public String getNumeroSocio() {
+        return NumeroSocio;
+    }
+    public void setNumeroSocio(String numeroSocio) {
+        NumeroSocio = numeroSocio;
+    }
+    public Integer getEdad() {
+        return Edad;
+    }
+    public void setEdad(Integer edad) {
+        Edad = edad;
+    }
+}
